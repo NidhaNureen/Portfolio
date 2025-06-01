bc57eb8651672e9d92bd00085a1b347c8a31a2db
NidhaNureen <nnurwork@gmail.com>
2025-06-01 21:12:58 +1200

    finished designing, started on main page now

--- folio.js
+++ folio.js
@@ -1,1 +1,19 @@
-const stickySections = [...document.querySelectorAll('.sticky')]
+
+function updateDateTime() {
+    var dateElem = document.getElementById('date');
+    var timeElem = document.getElementById('time');
+
+    const date = new Date();
+
+    const dateFormat = { weekday: 'short', day: 'numeric', month: 'short'};
+    const formattedDate = date.toLocaleDateString('en-GB', dateFormat);
+
+    const timeFormat = { hour: '2-digit', minute: '2-digit', hour12: true};
+    const formattedTime = date.toLocaleTimeString('en-GB', timeFormat).toUpperCase();
+
+    timeElem.textContent = formattedTime;
+    dateElem.textContent = formattedDate;
+}
+
+updateDateTime()
+setInterval(updateDateTime, 1000)
