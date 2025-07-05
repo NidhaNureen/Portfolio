6ce9a6034c3bdfe494e5f27689b57d6a8dfd63bc
NidhaNureen <nnurwork@gmail.com>
2025-07-05 17:09:14 +1200

    added other windows for sections

--- folio.js
+++ folio.js
@@ -20,3 +20,31 @@ function updateDateTime() {
 
 updateDateTime()
 setInterval(updateDateTime, 1000)
+
+/**
+ * Opens window when button is clicked
+ */
+function openWindow(windowId) {
+    var window = document.getElementById(windowId);
+    window.style.display = "flex";
+}
+
+document.getElementById("abt-me-button").addEventListener("click", () => openWindow('about-me-window'));
+document.getElementById("skills-button").addEventListener("click", () => openWindow('skills-window'));
+document.getElementById("projects-button").addEventListener("click", () => openWindow('projects-window'));
+document.getElementById("contact-button").addEventListener("click", () => openWindow('contact-window'));
+
+/**
+ * Closing the windows
+ */
+function closeWindow(windowId) {
+    var window = document.getElementById(windowId);
+    window.style.display = "none";
+}
+
+document.querySelectorAll(".close-button").forEach(button => {
+    button.addEventListener("click", () => {
+        var windowId = button.getAttribute('data-window');
+        closeWindow(windowId);
+    });
+});
